0b5a04da304a76173311e41f088170b37550a5af
Bakir Karovic <bakir.karovic@gmail.com>
2020-05-12 13:58:36 +0200

    updated base components to show menu icons

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCalendarCheck,
+  faCoffee,
+  faCompressAlt,
+  faCopy,
+  faFile,
+  faHandshake,
+  faIdCard,
+  faServer,
+  faShareSquare,
+  faUserCog,
+} from '@fortawesome/free-solid-svg-icons';
 import { Menu } from 'src/app/models/model';
 import * as menu from 'src/assets/menu/menu.json';
 
@@ -10,5 +21,16 @@ import * as menu from 'src/assets/menu/menu.json';
 })
 export class NavbarComponent {
   menus: Menu[] = menu.menus;
-  faCoffee = faCoffee;
+  icons = {
+    faCoffee: faCoffee,
+    faCopy: faCopy,
+    faFile: faFile,
+    faHandshake: faHandshake,
+    faIdCard: faIdCard,
+    faUserCog: faUserCog,
+    faServer: faServer,
+    faShareSquare: faShareSquare,
+    faCalendarCheck: faCalendarCheck,
+    faCompressAlt: faCompressAlt,
+  };
 }
